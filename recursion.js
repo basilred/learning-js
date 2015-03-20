7f9e5d50f70067f1ccb86b3bf486b6a68f779285
Vassily Krasnov <mykrasnov@gmail.com>
2015-03-20 14:03:09 +0300

    Replace negative condition with isEven call when number less than 0.

--- recursion.js
+++ recursion.js
@@ -1,9 +1,10 @@
 function isEven(number) {
-    if (number < 0) return;
     if (number === 0) {
         return true;
     } else if (number === 1) {
         return false;
+    } else if (number < 0) {
+        return isEven(-number);
     } else {
         return isEven(number - 2);
     }
@@ -11,4 +12,4 @@ function isEven(number) {
 
 console.log(isEven(42));    //true
 console.log(isEven(13));    //false
-console.log(isEven(-3));    //undefined
+console.log(isEven(-3));    //false
