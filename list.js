cd6d25456729546c189b9cab88ace7230f6d3beb
Vassily Krasnov <mykrasnov@gmail.com>
2015-03-23 15:02:03 +0300

    Added nth function & console log.

--- list.js
+++ list.js
@@ -20,9 +20,18 @@ function prepend(value, list) {
     return { value: value, rest: list };
 }
 
+function nth(list, n) {
+    if (!list)
+        return undefined;
+    else if (n == 0)
+        return list.value;
+    else
+        return nth(list.rest, n - 1);
+}
 
-console.log(arrayToList([1,2,3, "four", "five", "six", 777]));
+var array = [1,2,3, "four", "five", "six", 777];
 
-console.log(listToArray(arrayToList([1,2,3, "four", "five", "six", 777])));
-
+console.log(arrayToList(array));
+console.log(listToArray(arrayToList(array)));
 console.log(prepend(10, prepend(20, null)));
+console.log(nth(arrayToList(array), 5));
